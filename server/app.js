c0a7c086ae990a2d56fe896f38838ea4099338d6
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-04-01 01:08:55 -0700

    Update the preflight response headers

--- server/app.js
+++ server/app.js
@@ -40,6 +40,10 @@ app.use(cors({
 // Automatically respond to OPTIONS (preflight) requests
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://click-clack-lime.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.sendStatus(204);
   } else {
     next();
