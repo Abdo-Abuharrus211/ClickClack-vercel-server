c6478e07e5d0e172a2e69e814da474318143639d
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-04-01 20:18:56 -0700

    remove extra

--- server/app.js
+++ server/app.js
@@ -14,8 +14,6 @@ import cors from 'cors';
 import sql from './db.js';
 import * as endpoints from './endpoints.js'
 import swaggerUiDist from 'swagger-ui-dist';
-import swaggerUi from 'swagger-ui-express';
-
 
 
 const app = express();
